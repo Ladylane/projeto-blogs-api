7a45a934573a924064bb6755386da03645ce6f2d
Ladylane <ladylanesousa@gmail.com>
2022-07-13 22:19:31 -0300

    Requisito 11

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory',
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+    },
+    { timestamps: false },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categoryId',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'postId',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
